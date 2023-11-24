09071e1ff08ac6de67460df07a68b55ab7a9eba5
Steven Benner <admin@stevenbenner.com>
2023-11-24 15:30:35 -0800

    Created comment display system for old post comments

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,6 +15,25 @@ const postsCollection = defineCollection({
 	})
 });
 
+const commentsCollection = defineCollection({
+	type: 'data',
+	schema: z.array(
+		z.object({
+			author: z.number(),
+			author_name: z.string(),
+			author_url: z.string().optional(),
+			date_gmt: z.string(),
+			content: z.object({
+				rendered: z.string()
+			}),
+			author_avatar_urls: z.object({
+				48: z.string(),
+			})
+		})
+	)
+});
+
 export const collections = {
-	posts: postsCollection
+	posts: postsCollection,
+	comments: commentsCollection
 };
