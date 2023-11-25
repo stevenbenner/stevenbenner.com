b915e28bda24c65ddda501cd6fe502381dd35597
Steven Benner <admin@stevenbenner.com>
2023-11-25 12:53:14 -0800

    Create sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import { site } from './src/data/config.json';
 
 export default defineConfig({
-	site: site.url
+	site: site.url,
+	integrations: [
+		sitemap({
+			lastmod: new Date()
+		})
+	]
 });
